f7aeb07d67ca67cb6177a14a17119c4b8f6e0805
kamrana005 <kamranda@code.edu.az>
2022-05-30 12:29:39 +0400

    file created

---
+++ Homework30-05/Notebook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework30_05
+{
+    internal class Notebook
+    {
+        public string Name;
+        public string BrandName;
+        public int Price;
+    }
+}
---
+++ Homework30-05/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Homework30_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook()
+            {
+                Name = "Zenbook",
+                BrandName = "Asus",
+                Price = 4000
+
+            };
+            Notebook notebook2 = new Notebook()
+            {
+                Name = "Macbook",
+                BrandName = "Apple",
+                Price = 4400
+            };
+            Notebook notebook3 = new Notebook()
+            {
+                Name = "Ideapad",
+                BrandName = "Lenovo",
+                Price = 2200
+            };
+            Notebook notebook4 = new Notebook()
+            {
+                Name = "Yoga",
+                BrandName = "Lenovo",
+                Price = 1900
+            };
+            Notebook notebook5 = new Notebook()
+            {
+                Name = "Vivobook",
+                BrandName = "Asus",
+                Price = 1500
+            };
+            Notebook[] notebooks = new Notebook[5];
+            notebooks[0] = notebook1;
+            notebooks[1] = notebook2;
+            notebooks[2] = notebook3;
+            notebooks[3] = notebook4;
+            notebooks[4] = notebook5;
+
+
+
+            Console.WriteLine("Minimum qiymeti daxil edin:");
+            
+            int MinPrice = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Maksimum qiymeti daxil edin:");
+            
+            int MaxPrice = Convert.ToInt32(Console.ReadLine());
+            
+            
+                
+            
+            
+            for (int i = 0; i < notebooks.Length; i++)
+            {
+                if (notebooks[i].Price >= MinPrice)  
+                {
+                    if (notebooks[i].Price <= MaxPrice)
+                    {
+                        Console.WriteLine(notebooks[i].Name);
+                        
+                    }
+                    
+                    
+                    
+                    
+                      
+                    
+                }
+
+
+            }
+            
+            
+                
+
+                
+            
+
+                
+                
+                
+            
+
+            
+                
+        }
+        
+
+
+
+
+
+
+    }
+}
